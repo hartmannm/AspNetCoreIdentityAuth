05c898ed34c9e23260e432ae22fddf7509f8b77b
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-09 20:58:27 -0300

    CQRS - comandos
    - Criada classe base para os comandos
    - Criada classe para padronizar os retornos dos comandos
    - Criada classe base para o executor do comando

---
+++ src/Shared/ANCIA.Core/Messages/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using MediatR;
+
+namespace ANCIA.Core.Messages.Commands
+{
+    public abstract class Command : IRequest<CommandResult>
+    {
+        public DateTime CreatedAt { get; private set; }
+        public ValidationResult ValidationResult { get; protected set; }
+
+        public Command()
+        {
+            CreatedAt = DateTime.Now;
+            ValidationResult = new ValidationResult();
+        }
+
+        public virtual bool IsValid() => ValidationResult.IsValid;
+    }
+}
---
+++ src/Shared/ANCIA.Core/Messages/Commands/CommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using ANCIA.Core.Notifications;
+
+namespace ANCIA.Core.Messages.Commands
+{
+    public abstract class CommandHandler
+    {
+        protected INotifier _notifier;
+
+        protected CommandHandler(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+    }
+}
