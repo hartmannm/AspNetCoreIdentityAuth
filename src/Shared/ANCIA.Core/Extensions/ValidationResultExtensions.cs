be08a0d2961bab5b0c49f8afc1a1eeb60f7655a2
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-09 20:54:47 -0300

    Extensão das validações
    Criada extensão para converter erros do Fluent em notificações

---
+++ src/Shared/ANCIA.Core/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ANCIA.Core.Notifications;
+using FluentValidation.Results;
+
+namespace ANCIA.Core.Extensions
+{
+    public static class ValidationResultExtensions
+    {
+        public static IEnumerable<Notification> ToNotificationList(this ValidationResult validationResult)
+        {
+            return validationResult.Errors
+                    .Select(error => new Notification(error.ErrorMessage))
+                    .ToList();
+        }
+    }
+}
