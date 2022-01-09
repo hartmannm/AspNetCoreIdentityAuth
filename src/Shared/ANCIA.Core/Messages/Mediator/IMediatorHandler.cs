c38e316854abe6b46f418e8e72aba866f03672a1
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-09 20:59:38 -0300

    CQRS - Mediator
    - Criada interface para envio dos comandos ao executor
    - Criada implementação para envio do comando ao responsável pela execução

---
+++ src/Shared/ANCIA.Core/Messages/Mediator/IMediatorHandler.cs
@@ -0,0 +1,9 @@
+﻿using ANCIA.Core.Messages.Commands;
+
+namespace ANCIA.Core.Messages.Mediator
+{
+    public interface IMediatorHandler
+    {
+        Task<CommandResult> SendCommand<T>(T command) where T : Command;
+    }
+}
---
+++ src/Shared/ANCIA.Core/Messages/Mediator/MediatorHandler.cs
@@ -0,0 +1,21 @@
+﻿using ANCIA.Core.Messages.Commands;
+using MediatR;
+
+namespace ANCIA.Core.Messages.Mediator
+{
+    public class MediatorHandler : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<CommandResult> SendCommand<T>(T command) where T : Command
+        {
+            var result = await _mediator.Send(command);
+            return result != null ? new CommandResult(result) : CommandResult.Empty();
+        }
+    }
+}
