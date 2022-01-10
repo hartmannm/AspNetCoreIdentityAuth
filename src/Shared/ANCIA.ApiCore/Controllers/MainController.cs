f910e6c9a242dd35baa6b827e48d62beab166211
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-09 21:01:25 -0300

    Controller genérico
    - Criada classe para padronizar os retornos das requisições
    - Criado controller genérico com métodos uteis a todos os controllers

---
+++ src/Shared/ANCIA.ApiCore/Controllers/MainController.cs
@@ -0,0 +1,38 @@
+﻿using ANCIA.ApiCore.Controllers.Models;
+using ANCIA.Core.Notifications;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANCIA.ApiCore.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        protected DefaultResult CreateSuccessEmptyReturn()
+        {
+            return new DefaultResult(true);
+        }
+
+        protected DefaultResult CreateSuccessReturn(object data)
+        {
+            return new DefaultResult(data);
+        }
+
+        protected DefaultResult CreateErrorReturn(IEnumerable<string> erros)
+        {
+            return new DefaultResult(erros);
+        }
+
+        protected IEnumerable<string> getErrorList(ValidationResult result)
+        {
+            return result.Errors
+                    .Select(e => e.ErrorMessage);
+        }
+
+        protected IEnumerable<string> getErrorList(IEnumerable<Notification> notifications)
+        {
+            return notifications.Select(notification => notification.Message)
+                .ToList();
+        }
+    }
+}
---
+++ src/Shared/ANCIA.ApiCore/Controllers/Models/DefaultResult.cs
@@ -0,0 +1,26 @@
+﻿namespace ANCIA.ApiCore.Controllers.Models
+{
+    public class DefaultResult
+    {
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public DefaultResult(bool success)
+        {
+            Success = success;
+        }
+
+        public DefaultResult(object data)
+        {
+            Success = true;
+            Data = data;
+        }
+
+        public DefaultResult(IEnumerable<string> errors)
+        {
+            Success = false;
+            Errors = errors;
+        }
+    }
+}
