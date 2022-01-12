fca1e9246e799dfdb9bab7ec59e100c25219902a
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-11 22:10:21 -0300

    Adicionados atributos de auditoria nas roles
    - Adicionados atributos de identificação de criação e atualização nas roles
    - Adicionado atributo para identificar se a role está ativa
    - Criada classe de mapeamento da role para o EF
    - Criada migration para atualizar a role

--- src/Services/Auth/ANCIA.Authentication/Domain/Models/AppRole.cs
+++ src/Services/Auth/ANCIA.Authentication/Domain/Models/AppRole.cs
@@ -4,9 +4,32 @@ namespace ANCIA.Authentication.Domain.Models
 {
     public class AppRole : IdentityRole
     {
-        public AppRole(string name)
+        public string CreatedBy { get; private set; }
+        public DateTimeOffset CreatedAt { get; private set; }
+        public string? LastUpdatedBy { get; private set; }
+        public DateTimeOffset? LastUpdatedAt { get; private set; }
+        public bool? Active { get; private set; }
+
+        public AppRole(string name, string createdBy)
         {
             Name = name;
+            CreatedBy = createdBy;
+            CreatedAt = DateTimeOffset.Now;
+            Active = true;
+        }
+
+        public void AtualizarNome(string name, string updatedBy)
+        {
+            Name = name;
+            LastUpdatedBy = updatedBy;
+            LastUpdatedAt = DateTimeOffset.Now;
+        }
+
+        public void Inativar(string updatedBy)
+        {
+            Active = false;
+            LastUpdatedBy = updatedBy;
+            LastUpdatedAt = DateTimeOffset.Now;
         }
     }
 }
---
+++ src/Services/Auth/ANCIA.Authentication/Migrations/20220112010550_Adicionados_Atributos_Auditoria_Role.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ANCIA.Authentication.Migrations
+{
+    public partial class Adicionados_Atributos_Auditoria_Role : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: true,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "AspNetRoles",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AspNetRoles",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastUpdatedAt",
+                table: "AspNetRoles",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "AspNetRoles",
+                type: "varchar(255)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_CreatedBy",
+                table: "AspNetRoles",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_LastUpdatedBy",
+                table: "AspNetRoles",
+                column: "LastUpdatedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_CreatedBy",
+                table: "AspNetRoles",
+                column: "CreatedBy",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_LastUpdatedBy",
+                table: "AspNetRoles",
+                column: "LastUpdatedBy",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_CreatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_LastUpdatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_CreatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_LastUpdatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedAt",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "AspNetRoles");
+        }
+    }
+}
