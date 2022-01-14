54050294af265f61b8df688825ac38c5b12d3650
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-01-13 21:07:51 -0300

    Teste unitários
    - Criados testes unitários para o comando de criar usuário
    - Criados testes unitários para a validação de senha

---
+++ tests/Services/ANCIA.AuthenticationTests/Application/Commands/CreateUserCommandTest.cs
@@ -0,0 +1,79 @@
+﻿using ANCIA.Authentication.Application.Commands;
+using Xunit;
+
+namespace ANCIA.AuthenticationTests.Application.Commands
+{
+    public class CreateUserCommandTest
+    {
+        [Fact]
+        public void Deve_RetornarValido_Quando_EmailForValido()
+        {
+            var command = new CreateUserCommand
+            {
+                Email = "teste@teste.com",
+                Password = "Teste@123",
+                ConfirmPassword = "Teste@123"
+            };
+            Assert.True(command.IsValid());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("testeteste.com")]
+        public void Deve_RetornarErro_Quando_EmailFormatoInvalido(string email)
+        {
+            var command = new CreateUserCommand
+            {
+                Email = email,
+                Password = "Teste@123",
+                ConfirmPassword = "Teste@123"
+            };
+            Assert.False(command.IsValid());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("Teste@1")]
+        [InlineData("Teste123")]
+        [InlineData("Teste@@@")]
+        [InlineData("teste@123")]
+        public void Deve_RetornarErro_Quando_SenhaInvalida(string senha)
+        {
+            var command = new CreateUserCommand
+            {
+                Email = "teste@teste.com",
+                Password = senha,
+                ConfirmPassword = senha
+            };
+            Assert.False(command.IsValid());
+        }
+
+        [Theory]
+        [InlineData("Teste@123", null)]
+        [InlineData("Teste@123", "Teste@321")]
+        public void Deve_RetornarErro_Quando_SenhasDiferentes(string password, string confirmPassword)
+        {
+            var command = new CreateUserCommand
+            {
+                Email = "teste@teste.com",
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+            Assert.False(command.IsValid());
+        }
+
+        [Fact]
+        public void Deve_RetornarValido_Quando_SenhasIguais()
+        {
+            var command = new CreateUserCommand
+            {
+                Email = "teste@teste.com",
+                Password = "Teste@123",
+                ConfirmPassword = "Teste@123"
+            };
+            Assert.True(command.IsValid());
+        }
+    }
+}
---
+++ tests/Services/ANCIA.AuthenticationTests/Domain/Validations/PasswordValidationTest.cs
@@ -0,0 +1,41 @@
+﻿using ANCIA.Authentication.Domain.Validations;
+using Xunit;
+
+namespace ANCIA.AuthenticationTests.Domain.Validations
+{
+    public class PasswordValidationTest
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("teste12")]
+        public void Deve_RetornarErro_Quando_SenhaTamanhoInvalido(string password)
+        {
+            Assert.False(PaswordValidation.MinLength(password));
+        }
+
+        [Theory]
+        [InlineData("teste123")]
+        [InlineData("teste1234")]
+        public void Deve_RetornarSucesso_Quando_SenhaTamanhoValido(string password)
+        {
+            Assert.True(PaswordValidation.MinLength(password));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Teste@1")]
+        [InlineData("Teste123")]
+        [InlineData("Teste@@@")]
+        [InlineData("teste@123")]
+        public void Deve_RetornarErro_Quando_SenhaFormatoInvalido(string password)
+        {
+            Assert.False(PaswordValidation.Pattern(password));
+        }
+
+        [Fact]
+        public void Deve_RetornarSucesso_Quando_SenhaFormatoValido()
+        {
+            Assert.True(PaswordValidation.Pattern("Teste@123"));
+        }
+    }
+}
